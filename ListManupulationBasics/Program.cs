fbf06994d616badb78aa5c288c9efd67552b3bbb
Remzi <remzitdf@abv.bg>
2021-02-16 09:52:02 +0200

    Add files via upload

---
+++ ListManupulationBasics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManupulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+            int index;
+            while (input != "end")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                if (command == "Add")
+                {
+                    numbers.Add(index = int.Parse(tokens[1]));
+                }
+                else if (command=="Remove")
+                {
+                    numbers.Remove(index = int.Parse(tokens[1]));
+                }
+                else if (command=="RemoveAt")
+                {
+                    numbers.RemoveAt(index = int.Parse(tokens[1]));
+                }
+                else if (command=="Insert")
+                {
+                    int numberToInsert = int.Parse(tokens[1]);
+                    int indexToInsert = int.Parse(tokens[2]);
+                    numbers.Insert(indexToInsert, numberToInsert);
+                }
+               input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
---
+++ MergingLists/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> second = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> final = new List<int>();
+
+            int limit = Math.Min(first.Count,second.Count);
+
+            for (int i = 0; i < limit; i++)
+            {
+                final.Add(first[i]);
+                final.Add(second[i]);
+            }
+
+            if ((first.Count != second.Count))
+            {
+                if (first.Count > second.Count)
+                {
+                    final.AddRange(AddRemainedValues(first, second));
+                }
+                else
+                {
+                    final.AddRange(AddRemainedValues(second, first));
+                }
+            }
+            Console.WriteLine(string.Join(" ",final));
+        }
+        private static List<int> AddRemainedValues(List<int> longerList, List<int> shorterList) {
+            if (longerList.Count<=shorterList.Count)
+            {
+                throw new ArgumentException();
+            }
+
+            List<int> result = new List<int>();
+            for (int i = shorterList.Count; i < longerList.Count; i++)
+            {
+                result.Add(longerList[i]);
+            }
+
+            return result;
+        }
+    }
+}
